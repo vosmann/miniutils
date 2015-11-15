8c9a1b1381aa1ec46245aa674060157b76f24107
Vjekoslav Osmann <vosmann@gmail.com>
2015-11-15 12:31:14 +0100

    Made the S3 address nice and wrote unit tests.

--- src/main/java/com/vosmann/miniutils/s3/Address.java
+++ src/main/java/com/vosmann/miniutils/s3/Address.java
@@ -3,6 +3,7 @@ package com.vosmann.miniutils.s3;
 import com.google.common.collect.Lists;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -11,35 +12,52 @@ import static java.util.stream.Collectors.joining;
 
 public class Address {
 
-    private static final String HTTP_PREFIX = "http://s3.amazonaws.com";
-    private static final String S3_PREFIX = "s3:/"; // Second slash comes as delim.
+    private static final String HTTP_PREFIX = "http://s3";
+    private static final String HTTP_SUFFIX = ".amazonaws.com";
     private static final String SLASH = "/";
+    private static final String DASH = "-";
 
     private final String bucket;
     private final String key;
+    private final Optional<String> region;
 
     public static Address of(final String bucket, final String key) {
         return new Builder().bucket(bucket).keyPart(key).build();
     }
 
+    public static Address of(final String bucket, final String key, final String region) {
+        return new Builder().bucket(bucket).keyPart(key).region(region).build();
+    }
+
     private Address(Builder builder) {
         bucket = builder.bucket;
         key = builder.keyParts.stream().collect(joining(SLASH));
+        region = builder.region;
+        checkArgument(!isNullOrEmpty(bucket), "Can't have a null/blank bucket.");
+        checkArgument(!isNullOrEmpty(key), "Can't have a null/blank key.");
+        checkArgument(!region.isPresent() || !isNullOrEmpty(region.get()), "Can't have a null/blank region.");
     }
 
     public static final class Builder {
         private String bucket;
         private List<String> keyParts = Lists.newArrayList();
+        private Optional<String> region = Optional.empty();
 
-        public Builder bucket(String bucket) {
+        public Builder bucket(final String bucket) {
             checkArgument(!isNullOrEmpty(bucket), "Can't set a null/blank bucket.");
             this.bucket = bucket;
             return this;
         }
 
-        public Builder keyPart(String keyPart) {
+        public Builder keyPart(final String keyPart) {
             checkArgument(!isNullOrEmpty(keyPart), "Can't add a null/blank key part.");
             this.keyParts.add(keyPart);
+            return this;
+        }
+
+        public Builder region(final String region) {
+            checkArgument(!isNullOrEmpty(region), "Can't add a null/blank region.");
+            this.region = Optional.of(region);
             return this;
         }
 
@@ -57,15 +75,16 @@ public class Address {
     }
 
     public String getHttpUrl() {
-        return Stream.of(HTTP_PREFIX, bucket, key).collect(joining(SLASH));
-    }
-
-    public String getS3Url() {
-        return Stream.of(S3_PREFIX, bucket, key).collect(joining(SLASH));
+        if (region.isPresent()) {
+            return HTTP_PREFIX + DASH + region.get() + HTTP_SUFFIX + SLASH + bucket + SLASH + key;
+        } else {
+            return HTTP_PREFIX + HTTP_SUFFIX + SLASH + bucket + SLASH + key;
+        }
     }
 
     @Override
     public String toString() {
-        return "Address{" + "bucket='" + bucket + ", key='" + key + '}';
+        return "Address{" + "bucket='" + bucket + '\'' + ", key='" + key + '\'' + ", region=" + region + '}';
     }
+
 }
--- src/test/java/com/vosmann/miniutils/s3/AddressTest.java
+++ src/test/java/com/vosmann/miniutils/s3/AddressTest.java
@@ -2,28 +2,77 @@ package com.vosmann.miniutils.s3;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class AddressTest {
 
-    // @Test(expected = IllegalArgumentException.class)
-    @Test
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullBucket() {
+        new Address.Builder().bucket(null).keyPart("key").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyBucket() {
+        new Address.Builder().bucket("").keyPart("key").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnsetBucket() {
+        new Address.Builder().keyPart("key").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullKey() {
+        new Address.Builder().bucket("buck").keyPart(null).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testEmptyKey() {
-        // new Address.Builder().bucket("buket").build();
+        new Address.Builder().bucket("buck").keyPart("").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnsetKey() {
+        new Address.Builder().bucket("buck").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullRegion() {
+        new Address.Builder().bucket("buck").keyPart("key").region(null).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyRegion() {
+        new Address.Builder().bucket("buck").keyPart("key").region("").build();
+    }
+
+    @Test
+    public void testUnsetRegion() {
+        new Address.Builder().bucket("buck").keyPart("key").build();
     }
 
     @Test
     public void testGetBucket() throws Exception {
+        assertThat(new Address.Builder().bucket("b1").keyPart("k1").build().getBucket(), is("b1"));
     }
 
     @Test
     public void testGetKey() throws Exception {
+        assertThat(new Address.Builder().bucket("b1").keyPart("k1").build().getKey(), is("k1"));
+        assertThat(new Address.Builder().bucket("b1").keyPart("k1").keyPart("k2").build().getKey(), is("k1/k2"));
     }
 
     @Test
     public void testGetHttpUrl() throws Exception {
+        assertThat(new Address.Builder().bucket("b1").keyPart("k1").keyPart("k2").build().getHttpUrl(),
+                is("http://s3.amazonaws.com/b1/k1/k2"));
     }
 
     @Test
-    public void testGetS3Url() throws Exception {
+    public void testGetHttpUrlWithRegion() throws Exception {
+        assertThat(new Address.Builder().bucket("b1").keyPart("k1").region("eu-central-1").build().getHttpUrl(),
+                is("http://s3-eu-central-1.amazonaws.com/b1/k1"));
     }
 
 }
